c8add8dc9d90bd2c5e91f503a4c62d966eb7d434
cnoto <CicelyNoto@Cicelys-MacBook-Air.local>
2017-09-20 16:59:14 -0400

    Added button functionality
    Added a click listener to button and changed favorite to solid.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,43 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  cards = [
+    {
+      imgPath: "iceCream.jpg",
+      title: "Ice Creams",
+      desc: "Here are some cups of ice cream",
+      favorited: false
+    },
+
+    {
+      imgPath: "cupcakes.jpg",
+      title: "Cupcakes",
+      desc: "Here are some cupcakes",
+      favorited: false
+    },
+
+    {
+      imgPath: "sundae.jpg",
+      title: "Hot Fudge Sundae",
+      desc: "Here is a cup of ice cream sundae",
+      favorited: false
+    }
+  ]
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  clickFav(card) {
+    let index = this.cards.indexOf(card);
+
+    if(index > -1){
+      if(card.favorited){
+         this.cards[index].favorited = false;
+      }else{
+        this.cards[index].favorited = true;
+      }
+    }
   }
 
 }
